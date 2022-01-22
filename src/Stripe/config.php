a50f9a1089413f1d8e5705a9b0e79b7bbc58cd78
Martindev <Delebecque.martin@gmail.com>
2022-01-22 23:21:52 +0100

    Update config.php

--- src/Stripe/config.php
+++ src/Stripe/config.php
@@ -3,6 +3,7 @@
 use App\Stripe\Api\Entity\StripeUser;
 use App\Stripe\Api\Stripe;
 use App\Stripe\StripePaymentType;
+use App\Stripe\StripePaymentBoard;
 
 use function DI\add;
 use function DI\autowire;
@@ -12,7 +13,7 @@ return [
     'auth.entity'       => StripeUser::class,
     'payments.type'     => add(get(StripePaymentType::class)),
     'csrf.except'       => add(['stripe.webhook']),
-    'payment.boards'    => add(get(StripePaymentType::class)),
+    'payment.boards'    => add(get(StripePaymentBoard::class)),
     Stripe::class       => autowire()
         ->constructorParameter('endpointkey', $_ENV['STRIPE_ENDPOINT'] ?? null)
         ->constructorParameter('privateKey', $_ENV['STRIPE_SECRET'] ?? null)
